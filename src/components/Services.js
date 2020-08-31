89e62328c4159892f40badf0bc7de646f48555b7
Arpit Paliwal <apaliwal@localhost.localdomain>
2020-09-01 00:44:33 +0530

    Service component added

---
+++ src/components/Services.js
@@ -0,0 +1,43 @@
+import React, { Component } from 'react'
+import {FaCocktail, FaHiking, FaShuttleVan, FaBeer} from "react-icons/fa";
+import Title from "../components/Title"
+export default class Services extends Component {
+    state ={
+        services:[
+            {
+                icon:<FaCocktail/>,
+                title:"free cocktails",
+                info:"Free cocktail on arrival"
+            },
+            {
+                icon:<FaHiking/>,
+                title:"free Hiking",
+                info:"Free hiking for 1 hour"
+            },
+            {
+                icon:<FaShuttleVan/>,
+                title:"free Van",
+                info:"We have Shuttle van service in free"
+            },
+            {
+                icon:<FaBeer/>,
+                title:"free Beer",
+                info:"We are complemetary beer 1+1"
+            }
+        ]
+    }
+    render() {
+        return (
+            <section className="services">
+                <Title title="services"/>
+                <div className="services-center">{this.state.services.map((item, index) =>{
+                    return <article key={index} className="service">
+                        <span>{item.icon}</span>
+                       <h6>{item.title}</h6>
+                       <p>{item.info}</p>
+                    </article>
+                })}</div>
+            </section>
+        )
+    }
+}
--- src/pages/home.js
+++ src/pages/home.js
@@ -1,13 +1,17 @@
 import React from 'react'
 import Hero from "../components/Hero"
 import Banner from "../components/Banner"
+import Services from "../components/Services"
 import {Link} from "react-router-dom"
 export default function home() {
     return (
+        <>
        <Hero>
            <Banner title="luxurious rooms" subtitle="Delux rooms strating at 2999 Rs">
            <Link to="/rooms" className="btn-primary">Our rooms</Link>
            </Banner>
        </Hero>
+       <Services/>
+        </>
     )
 }
